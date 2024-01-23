6f598cb684b4c305cca00b227841f94b7fe1eafa
annieherieau <annie.herieau@gmail.com>
2024-01-23 21:59:45 +0100

    ceasar + test ok

--- lib/02_caesar_cipher.rb
+++ lib/02_caesar_cipher.rb
@@ -1,2 +1,40 @@
 # frozen_string_literal: true
 
+# verifier la clé : ingerger [0..26] >> returns boolean 
+def is_valid_key(key)
+  key.class == Integer && key.between?(0 , 26)
+end
+
+# code_each_caracter letter with key
+def ciph_each_caracter(car, key)
+  if /[[:alpha:]]/.match?(car)
+    car_ascii = car.ord + key
+    # A-Z : 65 à 90 / a-z : 97-122
+    car_ascii.between?("Z".ord+1, "a".ord+1) || car_ascii > "z".ord ? back_to_a = -26 : back_to_a = 0
+    ciphed_car = car_ascii + back_to_a
+    ciphed_car = ciphed_car.chr
+  else
+    ciphed_car = car
+  end
+  return ciphed_car
+end
+# chiffrer une string avec une clé 0 à 26
+def ceasar_cipher(str, key)
+  # verifier si la clé est valide
+  return "Clé acceptée : 0 à 26 inclus" unless is_valid_key(key)
+
+  # array of words
+  words = str.split
+
+  # Array of letters + chiph
+  ciphed_words = words.map do |word|
+    chars = word.chars.map do |car|
+      ciph_each_caracter(car, key)
+    end
+    chars.join
+  end 
+
+  # renvoyer la string
+  return ciphed_words.join(" ")
+end
+
--- spec/02_caesar_cipher_spec.rb
+++ spec/02_caesar_cipher_spec.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
-require_relative '../lib/01_caesar_cipher'
+require_relative '../lib/02_caesar_cipher'
+
+
+describe "is_valid_key function" do
+  it "returns true" do
+    expect(is_valid_key(3)).to eq(true)
+    expect(is_valid_key(26)).to eq(true)
+  end
+
+  it "returns false" do
+    expect(is_valid_key(-1)).to eq(false)
+    expect(is_valid_key(30)).to eq(false)
+    expect(is_valid_key("20")).to eq(false)
+    expect(is_valid_key(2.4)).to eq(false)
+  end
+
+end
+
+describe "ciph_each_caracter(car, key) function" do
+  it "returns a cripted letter (downcase)" do
+    expect(ciph_each_caracter("a", 3)).to eq("d")
+  end
+
+  it "returns a cripted letter (upcase)" do
+    expect(ciph_each_caracter("B", 3)).to eq("E")
+  end
+
+  it "returns a cripted letter (circle alphabet)" do
+    expect(ciph_each_caracter("x", 3)).to eq("a")
+  end
+
+  it "returns a non alpha caracter" do
+    expect(ciph_each_caracter("#", 3)).to eq("#")
+  end
+
+end
 
 describe "ceasar cipher function" do
-  it "returns a string" do
-    expect(ceasar_cipher("What a string!", 5)).to ("Bmfy f xywnsl!")
+  it "returns an error" do
+    expect(ceasar_cipher("What a string!", 30)).to eq("Clé acceptée : 0 à 26 inclus")
+  end
+  it "returns a coded string" do
+    expect(ceasar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
   end
 end
