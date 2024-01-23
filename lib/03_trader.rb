a113b4a11c050cc18bfcd253d3ec24da18e6b47b
annieherieau <annie.herieau@gmail.com>
2024-01-23 23:44:44 +0100

    trader + test ok

--- lib/03_trader.rb
+++ lib/03_trader.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+
+#
+def day_trader(prices_array)
+  prices = prices_array
+  sorted_prices = prices.sort
+  # array of best_buying_day already tested
+  best_buying_days = {}
+  
+  i_max = prices.length-1
+
+  sorted_prices.each do |best_buy|
+    i_buy = prices.index(best_buy)
+    #best to sell
+    best_sell = prices.slice(i_buy , i_max).max
+    i_sell = prices.index(best_sell)
+    #best deal
+    best_deal = best_sell - best_buy
+
+    best_buying_days[best_deal] = [i_buy , i_sell]
+  end
+
+  return best_buying_days[best_buying_days.keys.max]
+
+end
--- spec/03_trader_spec.rb
+++ spec/03_trader_spec.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require_relative '../lib/03_trader'
+
+describe "day_trader(prices) function" do
+  it "returns best buying day and best selling day" do
+    expect(day_trader([17, 3, 6, 9, 15, 8, 6, 1, 10])).to eq([1,4]) # 15 - 3 = 12
+  end
+end
