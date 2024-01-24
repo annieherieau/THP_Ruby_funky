88f78a1c3cfeda7e3e7b0aec7892b8fa0534efb8
annieherieau <annie.herieau@gmail.com>
2024-01-24 09:27:13 +0100

    swear word in shakespeare: file.txt

--- lib/04_word_counter.rb
+++ lib/04_word_counter.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
-dictionnary = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
+dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
+# read .txt file and slit into an array of words >> returns Array
+def file_to_words(file_path)
+  file = File.open(file_path, "r")
+  data = file.readlines.map(&:chomp)
+  words = Array.new
+  data.each {|string| words.push(string.gsub(/[[:punct:]]/, ' ').split)}
+  return words.flatten
+end
+
+# counter words occurences of dictionnary in corpus >> returns Hash
+# c'est avace un match donc c'est pas les mots en entiers...
+def word_counter(corpus, dictionnary)
+  # Array of sorted words
+  if corpus.class == String
+    words = corpus.gsub(/[[:punct:]]/, ' ').split.sort.uniq
+  else
+    words = corpus
+  end
+
+  result = Hash.new
+  
+  dictionnary.each do |dico|
+    words.each do |word|
+      if word.downcase.match?(dico)
+        result.key?(dico) ? result[dico] += 1 : result[dico] = 1
+      end
+    end
+  end
+  return result
+end
+
+def print_hash(hash)
+  hash.each_pair{|k,v| puts "#{k} : #{v} occurence(s)"}
+end
+
+# print the hash >> puts String
+print_hash(word_counter(file_to_words('lib/shorter_shakespeare.txt'), file_to_words('lib/swear_words.txt')))
--- spec/04_word_counter_spec.rb
+++ spec/04_word_counter_spec.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 require_relative '../lib/04_word_counter'
-
 dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
 describe "word_counter(corpus, dictionnary) function" do
   it "returns occurrences of a word" do
-    expect(("below", dictionnary)).to eq({"below"=>1, "low"=>1})
+    expect(word_counter("below", dictionnary)).to eq({"below"=>1, "low"=>1})
   end
 
   it "returns occurrences of words in a sentence" do
-    expect(("Howdy partner, sit down! How's it going?", dictionnary)).to eq({"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1})
+      expect(word_counter("Howdy partner, sit down! How's it going?", dictionnary)).to eq({"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1})
   end
 end
+
+#TODO: faire les tests pour file_to_words(file_path)
+#TODO: faire les tests pour print_hash(hash)
 
 
